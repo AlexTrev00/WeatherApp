a20294f55bef74e03fec077cdf38fc1cadc59b4e
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-10-24 14:22:52 -0600

    update de la GUI

--- GUI.py
+++ GUI.py
@@ -1,53 +1,58 @@
-from tkinter import *
-import tkinter.ttk
-from tkinter import font
-from tkinter import PhotoImage
-import main
-
-# funcion de degradado de color de la ventana
-def degradado (canvas, width, height, color1, color2):
-    for i in range(height):
-        r = int(color1[0]+(color2[0]- color1[0])*i/height)
-        g = int(color1[1]+(color2[1]-color1[1])*i/height)
-        b = int(color1[2]+(color2[2]- color1[2])*i/height)
-
-        color = f"#{r:02x}{g:02x}{b:02x}"
-        canvas.create_line(0, i, width, i, fill=color)
-
-ventana = tkinter.Tk()
-# creacion del tipo de fuente
-fuente = font.Font(family="Noto Sans JP", size=12, weight="normal")
-
-# titulo de la ventana
-ventana.title("Clima")
-
-# icono de la GUI
-ventana.iconbitmap("icons8-clima-16.ico")
-
-# etiqueta para agregar texto
-''' color_fondo01 = "#%02x%02x%02x" % (93, 109, 126) # se define el color tipo RGB
-etiqueta = tkinter.Label(ventana, text="Clima", fg="black", font=fuente, bg=color_fondo01) 
-etiqueta.pack() '''
-
-#boton lupa img 
-lupa_ruta="lupa.png"
-lupa = PhotoImage(file=lupa_ruta)
-ventana.lupa=lupa
-boton_lupa = tkinter.Button(ventana, image=lupa)
-boton_lupa.pack()
-
-# dimension de la ventana 
-width = 500
-height = 500
-ventana.geometry(f"{width}x{height}") # dimension de entrada de la ventana
-
-# se establace el color del degradado y se manda a llamar a la funcion
-canvas = tkinter.Canvas(ventana, width=width, height=height)
-canvas.pack()
-color1 = (93, 109, 126)
-color2 = (40, 55, 71 )
-degradado(canvas, width, height, color1, color2)
-
-ventana.mainloop()
+import flet as ft
+from main import temperatura_de_lugar, clima_lugar_icono
 
 
+class SearchButton(ft.ElevatedButton):
+    def __init__(self, text, on_click):
+        super().__init__()
+        self.bgcolor="#002b6e"
+        self.color="#ffffff"
+        self.on_click=on_click
+        self.text = text
+        self.icon= ft.icons.SEARCH
+
+
+def main(page: ft.Page):
+    page.bgcolor="#ffffff"
+    page.appbar = ft.AppBar(
+        # se envuelve todo el text en una fila para poder centrarlo con alignment
+        title= ft.Row (             
+            controls=[
+                ft.Text("Weather App", color="#ffffff"),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        bgcolor="#002b6e",
+    )
+    txt_field = ft.TextField(text_align=ft.TextAlign.LEFT, width=300, height=40, color="#002b6e")
+    def ok_clicked(e):
+        place = txt_field.value
+        if place:
+            temperature = temperatura_de_lugar(place)
+            icon_weather= clima_lugar_icono(place)
+
+            page.add (
+                ft.Row (
+                controls = [
+                   ft.Text(temperature, color="#002b6e"),
+                  ft.Image(icon_weather)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+                ),
+             ),
+
+    page.add(
+        ft.Row (
+            [
+                SearchButton(text='Search',on_click=ok_clicked),
+                txt_field
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+            
+
+        )
+        
+    )
+
+
+ft.app(main)
