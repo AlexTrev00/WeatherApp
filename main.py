eaf0a1a778766e02ad290340efd91b92340dfc2b
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-10-28 17:40:32 -0600

    funciones nuevas
    se le agrego la funcion de time_zone, wind_speed, wind_dir,

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import requests
+from datetime import datetime
+
 
 def temperatura_de_lugar (place):
-    parameters = {'key' : 'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place,
-                  'units':'metric', 'language': 'es'}
+    try:
+        parameters = {'key' : 'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place}
+        url = "https://www.meteosource.com/api/v1/free/point"
+        data = requests.get(url, parameters).json()
+        temp = data['current']['temperature']
+        return f"{temp} °C"
+    except:
+        print("Lugar geografico no accesible, intente de nuevo")
+def icono_clima(place):
+    try:
+        parameters = {'key' : 'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place}
+        url = "https://www.meteosource.com/api/v1/free/point"
+        data = requests.get(url, parameters).json()
+        icono = data['current']['icon_num']
+        return f"https://www.meteosource.com/static/img/ico/weather/{icono}.svg"
+    except :
+        return 'imagen no disponible'
+    
+def time_zone (place):
+    params = {'key':'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place,}
     url = "https://www.meteosource.com/api/v1/free/point"
-    data = requests.get(url, parameters).json()
-    return f"la temperatura actual de {place} es {data["current"], ["temperature"]} °C"
+    try:
+        data = requests.get(url, params).json()
+        data_byhour = data.get("hourly", {}).get("data",[])
+        hour_str = data_byhour[0]["date"]
+        hour_obj = datetime.fromisoformat(hour_str)
+        return hour_obj.strftime("%H:%M")
+    except (requests.RequestException, KeyError, IndexError) as error:
+        return f"hora no disponible {error}" 
 
-def clima_lugar_icono (place):
-    parameters = {'key': 'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place,}
+def forecast (place):
+    params= {'key':'rxsjl8vt5wbc5x5twoz149tf8bfnbgzsm1c0vcnb', 'place_id': place} 
     url = "https://www.meteosource.com/api/v1/free/point"
-    data = requests.get(url, parameters).json()
-    return f"{data["current"],["icon"]}"
+    data = requests.get(url, params).json()
+    wind_speed= data['current']['wind']['speed']
+    wind_dir = data['current']['wind']['dir']
+    return f'{wind_speed} {wind_dir}'
 
-
-
-
-if __name__ == "__main__":
-    temperatura_de_lugar()
-    clima_lugar_icono()
+#if __name__ == "__main__":
+ #   print (temperatura_de_lugar('japan'))
+  #  print (clima_lugar_icono('japan'))
