7f301487fe1b55262118dc08fcd382b5c63ae463
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-10-28 17:42:01 -0600

    GUI nueva version
    se le agrego formato de salida de las llamadas a los requests

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 import flet as ft
-from main import temperatura_de_lugar, clima_lugar_icono
+from main import temperatura_de_lugar, time_zone, forecast, icono_clima
 
 
 class SearchButton(ft.ElevatedButton):
@@ -10,36 +10,86 @@ class SearchButton(ft.ElevatedButton):
         self.on_click=on_click
         self.text = text
         self.icon= ft.icons.SEARCH
-
+class requestStyle ():
+    def __init__(self, text):
+        super().__init__()
+        self.color= '#ffffff'
+        self.font_family = 'Noto Sans Japanese'
+        self.size = 16
+        self.text = text
+        
 
 def main(page: ft.Page):
+    page.window.width=600
+    page.window.height=550
     page.bgcolor="#ffffff"
+    title_app = ft.Text("Weather App", color="#ffffff")
+    time =  ft.Text("", color="#ffffff")
     page.appbar = ft.AppBar(
         # se envuelve todo el text en una fila para poder centrarlo con alignment
         title= ft.Row (             
             controls=[
-                ft.Text("Weather App", color="#ffffff"),
+                title_app,
+                time
             ],
             alignment=ft.MainAxisAlignment.CENTER
         ),
         bgcolor="#002b6e",
     )
     txt_field = ft.TextField(text_align=ft.TextAlign.LEFT, width=300, height=40, color="#002b6e")
+    content_container = ft.Column()
     def ok_clicked(e):
         place = txt_field.value
         if place:
-            temperature = temperatura_de_lugar(place)
-            icon_weather= clima_lugar_icono(place)
+            txt_field.value = ""
+            content_container.controls.clear()
 
-            page.add (
-                ft.Row (
-                controls = [
-                   ft.Text(temperature, color="#002b6e"),
-                  ft.Image(icon_weather)
+            temperature = temperatura_de_lugar(place)
+            icon_url = icono_clima(place)
+            timezone = time_zone(place)
+            daily_time = forecast(place)
+
+            time.value= f"{timezone} {place}"
+            page.appbar.update()
+            
+
+            content_row = ft.Row (
+                controls=[
+                    ft.Container(
+                        content=ft.Text(temperature, color="#ffffff", font_family='Noto Sans Japanese', size=16, weight=ft.FontWeight.BOLD),
+                        padding=20,
+                        bgcolor='#234b76',
+                        border_radius=10,
+                        margin=20,
+                    ),
+                    ft.Container(
+                        content=ft.Image(src=icon_url, width=60, height=60),
+                        padding=20,
+                        bgcolor='#234b76',
+                        border_radius=10,
+                        margin=20,
+                    ),
+                    ft.Container(
+                        content= ft.Column (
+                            controls = [
+                                ft.Text('Wind Speed', color='#ffffff', font_family='Noto Sans Japanese', size=16, weight=ft.FontWeight.BOLD),
+                                ft.Text(daily_time , color='#ffffff', font_family='Noto Sans Japanese', size=16, weight=ft.FontWeight.BOLD),
+                            ],
+                        ),
+                        padding=20,
+                        bgcolor='#234b76',
+                        border_radius=10,
+                        margin=20,
+                    )
                 ],
                 alignment=ft.MainAxisAlignment.CENTER
-                ),
-             ),
+            )
+            #limpia el contenido anterior para mostrar la nueva solicitud 
+            content_container.controls.append(content_row)
+            page.add(content_container)
+            page.update()
+            
+            
 
     page.add(
         ft.Row (
