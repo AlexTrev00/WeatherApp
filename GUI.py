e2131394f654a4a2432e1d0e8bbc8fe4fc051205
Alex.Trev00 <89210446+AlexTrev00@users.noreply.github.com>
2024-11-07 09:05:00 -0600

    v4.0
    adicion del boton para generar el reporte de excel

--- GUI.py
+++ GUI.py
@@ -1,5 +1,8 @@
 import flet as ft
 from main import temperatura_de_lugar, time_zone, forecast, icono_clima, name_icono, regex
+from excel_functions import guardar_en_excel
+
+
 
 
 class SearchButton(ft.ElevatedButton):
@@ -17,6 +20,14 @@ class requestStyle ():
         self.font_family = 'Noto Sans Japanese'
         self.size = 16
         self.text = text
+class excelButton(ft.ElevatedButton):
+    def __init__(self, text, on_click):
+        super().__init__()
+        self.bgcolor="#002b6e"
+        self.color="#ffffff"
+        self.on_click=on_click
+        self.text = text
+
         
 
 def main(page: ft.Page):
@@ -39,27 +50,31 @@ def main(page: ft.Page):
     content_container = ft.Column()
     txt_field = ft.TextField(text_align=ft.TextAlign.CENTER, width=300, height=40, color="#002b6e")
     
-
+    btn_excel = excelButton(text='Excel Report', on_click=lambda e:excel_funcion(e))
+    btn_excel.visible = False
     def ok_clicked(e):
-        
         place = txt_field.value
         if place:
             reg = regex(place)
-            # validacion de la expresion regular en FLET
+            # condicional de la funcion regex 
             if reg != True:
                 secondary_row = ft.Row (
                     controls=[
                         ft.Container(
-                            content=ft.Text('Lo Siento, Solo se admiten letras, intentelo de nuevo', color="ffd800", font_family="Noto Sans Japanese", size=16, weight=ft.FontWeight.BOLD),
+                            content=ft.Text('Lo Siento, solo se admiten letras, intente de nuevo', color="#ffd800", font_family='Noto Sans Japanese', size=16, weight=ft.FontWeight.BOLD),
                             padding=20,
                             bgcolor='#234b76',
-                            border_radius = 10,
+                            border_radius=10,
                             margin=20,
                         )
                     ],
-                    alignment = ft.MainAxisAlignment.CENTER
+                    alignment=ft.MainAxisAlignment.CENTER
                 )
-            txt_field.value = ""
+                content_container.controls.append(secondary_row)
+                page.add(content_container)
+                page.update()
+            
+            #txt_field.value = ""
             content_container.controls.clear()
 
             temperature = temperatura_de_lugar(place)
@@ -67,6 +82,7 @@ def main(page: ft.Page):
             timezone = time_zone(place)
             daily_time = forecast(place)
             icon_description = name_icono(place)
+            
 
             time.value= f"{timezone} {place.capitalize()}"
             page.appbar.update()
@@ -92,7 +108,7 @@ def main(page: ft.Page):
                         padding=20,
                         bgcolor='#234b76',
                         border_radius=10,
-                        margin=20,
+                        margin=20
                     ),
                     ft.Container(
                         content= ft.Column (
@@ -108,13 +124,29 @@ def main(page: ft.Page):
                     )
                 ],
                 alignment=ft.MainAxisAlignment.CENTER
-            )
+            )     
             #limpia el contenido anterior para mostrar la nueva solicitud 
             content_container.controls.append(content_row)
+            btn_excel.visible=True
+            btn_excel_row = ft.Row(
+                controls=[
+                    btn_excel
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+            content_container.controls.append(btn_excel_row)
             page.add(content_container)
             page.update()
-            
-            
+    def excel_funcion(e):
+        place = txt_field.value
+        guardar_en_excel(place)
+        
+
+    def btn_visibility():
+        btn_excel.visible = bool(txt_field.value)
+        page.update()
+    
+    txt_field.on_change = btn_visibility
 
     page.add(
         ft.Row (
